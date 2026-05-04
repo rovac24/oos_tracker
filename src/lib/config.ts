5dcf6b6201e4740337b0bbad21efd87c949e55c4
rovac <rovac24@gmail.com>
2026-05-04 15:31:46 -0600

    update may spreadsheet tab

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,14 +3,14 @@ export const CLIENTS = [
     id: 'ascend',
     name: 'Ascend',
     icon: '/ascend-icon.png',
-    scriptUrl: 'https://script.google.com/macros/s/AKfycbw76WmBGPL5W3IiqImcqV_dGVEF896NRpiazY_uFdHa0sfCKAKZj4bbrOedAdzKLsRI/exec',
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbxi36akj5AZjRMrQ2zgHJGDvemLU-GiJ7IAtJeQXUIdAxco6M6Kat7EPDQS7MZT1Kqi/exec',
     pacificStone: false,
   },
   {
     id: 'pacific-stone',
     name: 'Pacific Stone',
     icon: '/pacific-stone-icon.png',
-    scriptUrl: 'https://script.google.com/macros/s/AKfycby_yUgQBJpH4ARCEkgioAs1BJJJdY3Q_g6gEeDgtuY--6eN12-v4RVNtxuZ78oBzNI9/exec',
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbxNwfDle-ENUZAodzvOELjwmhiPNQyoh5qNZ62WZNTbY0MbqvxfCFt7U1d2rW8hb-CX/exec',
     pacificStone: true,
   },
 ]
